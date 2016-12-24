dcacd45359231cbd60007081b191b8e49352e932
Thibaut Renoncourt <thibaut.renoncourt@gmail.com>
2016-12-24 10:37:23 +0100

    Update GameObjectExtention.cs

--- GameObjectExtention.cs
+++ GameObjectExtention.cs
@@ -1,12 +1,10 @@
-public static class GameObjectExtension
- {
-     public static T AddComponentWithInit<T>(this GameObject obj, System.Action<T> onInit) where T : Component
-     {
-         bool oldState = obj.activeSelf;
-         obj.SetActive(false);
-         T comp = obj.AddComponent<T>();
-         if (onInit != null)
-             onInit(comp);
-         obj.SetActive(oldState);
-     }
- }
+public static T AddComponentWithInit<T>(this GameObject obj, System.Action<T> onInit) where T : Component
+{
+    bool oldState = obj.activeSelf;
+    obj.SetActive(false);
+    T comp = obj.AddComponent<T>();
+    if (onInit != null)
+        onInit(comp);
+    obj.SetActive(oldState);
+    return comp;
+}
